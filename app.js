6339078c1e8494df5a8b0bad8dcb69b34fcd5f88
Lewis Lu <agooday32@gmail.com>
2021-05-01 16:19:22 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
 const port = 3000
 
+//install mongoose
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true } )
+
+// install handlebars template engine 
+const exphbs = require('express-handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -18,7 +23,7 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world ')
+  res.render('index')
 })
 
 app.listen(port, () => {
