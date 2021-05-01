f13495bd2be6637d619bcbc58ae610763288d5da
Lewis Lu <agooday32@gmail.com>
2021-05-01 15:26:13 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true } )
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world ')
