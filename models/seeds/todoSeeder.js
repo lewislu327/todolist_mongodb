aedb06b7f12cd9ce19fdfc4412bb1a480e9f3593
lewislu327 <agooday32@gmail.com>
2021-08-12 15:21:31 +0800

    refactor: use async/await function to refactor

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -6,25 +6,26 @@ const db = require('../../config/mongoose')
 const User = require('../user')
 const bcrypt = require('bcryptjs')
 
-
-const SEED_USER= {
+const SEED_USER = {
   name: 'root',
-  email:'root@email.com',
-  password:'123456'
+  email: 'root@email.com',
+  password: '123456'
 }
 
 db.once('open', () => {
   bcrypt
     .genSalt(10)
-    .then(salt => bcrypt.hash(SEED_USER.password, salt))
-    .then(hash => User.create({
-      name: SEED_USER.name,
-      email: SEED_USER.email,
-      password: hash 
-    }))
-    .then(user => {
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash
+      })
+    )
+    .then((user) => {
       const userId = user._id
-      return Promise.all( Array.from({ length:10 }, (_, i) => Todo.create({ name:`name-${i}`, userId} )))
+      return Promise.all(Array.from({ length: 10 }, (_, i) => Todo.create({ name: `name-${i}`, userId })))
     })
     .then(() => {
       console.log('done!')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,17 +1,16 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-// 引用 Todo model
 const Todo = require('../../models/todo')
-// 定義首頁路由
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res) => {
   const userId = req.user._id
-  Todo.find({ userId })
-    .lean()
-    .sort({ name: 'asc'})
-    .then(todos => res.render('index', { todos: todos}))
-    .catch(error => console.error(error))
+
+  try {
+    const todos = await Todo.find({ userId }).lean().sort({ name: 'asc' })
+    return res.render('index', { todos })
+  } catch (error) {
+    console.error(error)
+  }
 })
 
-// 匯出路由器
-module.exports = router 
+module.exports = router
